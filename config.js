a2b70b7f37071fa5e7f67baf77e3f498a9900a8c
sansugsmart92-oss <sansugsmart92@gmail.com>
2026-09-16 20:55:49 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ATENDIX_CONFIG = {
   SUPABASE_URL: 'https://fhyqmxaonlrojnzavvkr.supabase.co',
-  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_iVRvKPKHLoAa5gyPleqbFA_d4f54Z7B'
+  SUPABASE_PUBLISHABLE_KEY: 'COLE_AQUI_A_CHAVE_PUBLICAVEL'
 };
