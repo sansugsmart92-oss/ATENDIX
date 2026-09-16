89a1bbfd8fd904628e1b053d0ccbb773efd3cadd
sansugsmart92-oss <sansugsmart92@gmail.com>
2026-09-16 19:50:09 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ATENDIX_CONFIG = {
-  SUPABASE_URL: "https://fhyqmxaonlrojnzavvkr.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://fhyqmxaonlrojnzavvkr.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_iVRvKPKHLoAa5gyPleqbFA_d4f54Z7B"
 };
